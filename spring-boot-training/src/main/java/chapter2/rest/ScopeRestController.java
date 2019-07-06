c5049d083773e97f18506345b578b8491339aa89
neal.ma <neal.ma.sh@gmail.com>
2019-07-06 10:52:10 +0800

    x

--- spring-boot-training/src/main/java/chapter2/rest/ScopeRestController.java
+++ spring-boot-training/src/main/java/chapter2/rest/ScopeRestController.java
@@ -6,11 +6,9 @@ import chapter2.scope.InstanceService;
 import chapter2.scope.SingletonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
